3de2619eae59234b89e8e863714be52073e4e4db
BrAvellar <b.avellar01@edu.pucrs.br>
2025-05-28 19:00:08 -0300

    [fix]: removed examples

--- src/app/[locale]/examples/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MonthCalendar } from '@/components/Statistics/Calendar/Calendar'
-
-export default function CalendarExamplePage() {
-  return (
-    <div className="w-full flex m-10">
-      <MonthCalendar />
-    </div>
-  )
-}
