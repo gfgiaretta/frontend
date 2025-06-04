9da8e11507f58429b4a69db683f29bcdf35d1966
Daniel Paim Frantz <danielpaimf@gmail.com>
2025-06-04 17:53:30 -0300

    fix(no-us):Fixed publication in post (#24)
    * fix(no-us):Fixed publication in post
    
    * shortened the code and made the id optional
    
    ---------
    
    Co-authored-by: Daniel Frantz <danielpaimf@edu.pucrs.br>

--- src/app/[locale]/(withMenu)/post/page.tsx
+++ src/app/[locale]/(withMenu)/post/page.tsx
@@ -5,12 +5,11 @@ import { useSearchParams } from 'next/navigation'
 import { Publication } from '@/components/Publication/Publication'
 
 export default function CreatePostPage() {
-    const searchParams = useSearchParams()
-    const exerciseId = searchParams.get('exerciseId')
+  const searchParams = useSearchParams()
+  const exerciseId = searchParams.get('exerciseId')
   return (
     <div className="w-screen h-screen justify-center items-center flex bg-background">
-      {exerciseId && <Publication exerciseId={exerciseId} />}
-      {exerciseId == null && <div>Erro</div>}
+      <Publication exerciseId={exerciseId || null} />
     </div>
   )
 }
