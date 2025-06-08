7b485511413c8b99de1326221155f9045faf5773
Varboff <vinirechboff@gmail.com>
2025-06-08 15:40:16 -0300

    feat: Add service to fetch comments

---
+++ src/services/CommentService.ts
@@ -0,0 +1,19 @@
+import { api } from '@/utils/api'
+import { getToken } from '@/utils/token'
+
+export interface CommentDTO {
+  comment_id: string
+  owner: {
+    name: string
+    profile_picture_url: string
+  }
+  content: string
+}
+
+export async function fetchComments(postId: string): Promise<CommentDTO[]> {
+  const token = getToken()
+  const response = await api(token).get('/comment', {
+    params: { post_id: postId },
+  })
+  return response.data
+}
